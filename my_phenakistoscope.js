8e95f9b2cb977ce0cb58f5dc48204f20e9dace7d
Rubbishcandan <132533266+Rubbishcandan@users.noreply.github.com>
2023-09-11 10:22:09 +1200

    Things work :)

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,7 +1,9 @@
 const SLICE_COUNT = 10;
 let angle = -180
-var StarLocationX = [-100,0,100]
-var StarLocationY = [-400,-500,-600,-700,-800]
+var StarLocationX = [-100,0,100,150,200,250,300]
+var StarLocationY = [-400,-500,-650,-750,-800,-900]
+let r = 600
+
 
 function setup_pScope(pScope) {
   pScope.output_mode(ANIMATED_DISK);
@@ -29,17 +31,22 @@ function setup_layers(pScope) {
 
   var layer3 = new PLayer(Stars);
   layer3.mode(RING);
-  layer3.set_boundary(300,1000);
-  
+  layer3.set_boundary(-400,1000);
 
+  var layer4 = new PLayer(Asteroids);
+  layer4.mode(SWIRL(1));
+  layer4.set_boundary(400,1000)
+  
 
 }
 
 function Moon(animation, pScope) {
-  let r = 600
+ 
   noFill()
   stroke(255)
+  strokeWeight(1)
   circle (0,0, r*2)
+
   strokeWeight(64)
   stroke('#909090')
   let x = r * cos(angle);
@@ -51,6 +58,9 @@ function Moon(animation, pScope) {
   ellipse(x - 10, y, 20, 20)
   ellipse(x + 15, y + 10, 15, 15)
   ellipse(x + 10, y - 15, 12.5, 12.5)
+ 
+  
+
 
 }
 
@@ -60,14 +70,26 @@ function Planet(x, y, animation, pScope) {
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
-
+  var start = color('#4287f5')
+  var end =  color('#000000')
+  let interA = lerpColor(start,end,0.3)
+  let interB = lerpColor(start,end,0.6)
+  let interC = lerpColor(start,end,0.9)
  
+  fill(255)
+  rect(-50, -325 - animation.wave() * 10, 100, 20) // .wave is a cosine wave btw
   
-  fill(66, 135, 245)
+  fill('#4287f5')
   noStroke();
-  arc(x, y, 600, 600, backgroundArcStart, backgroundArcEnd); // draws "pizza slice" in the background
- // fill('#0da61d')
-  noSmooth()
+  //arc(x, y, 600, 600, backgroundArcStart, backgroundArcEnd); // draws "pizza slice" in the background
+  fill(interC)
+  circle(0,0,600)
+  fill(interB)
+  circle(0,0,575)
+  fill(interA)
+  circle(0,0,550)
+  fill('#4287f5')
+  circle(0,0,525)
   fill('#667E2C')
   circle(0,0,500)
   fill('#928e85')
@@ -86,19 +108,31 @@ function Planet(x, y, animation, pScope) {
   triangle(0,275,-10,260,10,260)
   fill('#928e85')
   triangle (0,270,-10,250,10,250)
-  //lerp colour with circle rings 
+  
+  
 
-  fill(255)
-  rect(-50, -300 - animation.wave() * 10, 100, 20) // .wave is a cosine wave btw
+  
 
 }
 
 function Stars (x,y, animation, pScope){
 //offset with animation.wave or different frequency 
   
-  strokeWeight(15)
-  stroke(255)
-  point((StarLocationX[2]),(StarLocationY[3]))
-  point((StarLocationX[1],StarLocationY[3]))
 
+  strokeWeight(15 - animation.wave()* 15)
+  stroke(255)
+  scale (1 + animation.wave() * 0.01)
+  point((StarLocationX[2]),(StarLocationY[3]))
+  point((StarLocationX[1]),(StarLocationY[2]))
+  point((StarLocationX[0]),(StarLocationY[4]))
+  point((StarLocationX[3]),(StarLocationY[1]))
+  point((StarLocationX[3]),(StarLocationY[5]))
+}
+
+function Asteroids (x,y, animation, pScope){
+
+scale(1 - animation.wave() * 0.5)
+ fill(255)
+ ellipse(x,y,40,40)
+ 
 }
