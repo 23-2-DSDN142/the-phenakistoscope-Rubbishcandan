8ba8c472f93551dbf68485544520dc71643295dc
Rubbishcandan <132533266+Rubbishcandan@users.noreply.github.com>
2023-09-13 22:01:55 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,8 +1,8 @@
 const SLICE_COUNT = 12;
-let angle = -180
-var StarLocationX = [-100,0,100,150,200,250,300]
-var StarLocationY = [-400,-500,-650,-750,-800,-900]
-let r = 600
+let angle = -180//angle for calculation, -180 do not change
+var StarLocationX = [-100,0,100,150,200,250,300]//array to make stars easier to place
+var StarLocationY = [-400,-500,-650,-750,-800,-900]//as above
+let r = 600 //radius for calculations, 600 do not change
 
 
 function setup_pScope(pScope) {
@@ -20,20 +20,20 @@ function setup_layers(pScope) {
   new PLayer(null, 0); //lets us draw the whole circle background, ignoring the boundaries
 
 
-  var layer1 = new PLayer(Moon);
+  var layer1 = new PLayer(Moon);// draws the orbiting moon 
   layer1.mode(RING);
   layer1.set_boundary(200, 1000);
 
-  var layer2 = new PLayer(Planet);
+  var layer2 = new PLayer(Planet);//draws the central planet and black holes 
   layer2.mode(RING);
   layer2.set_boundary(0, 400);
 
 
-  var layer3 = new PLayer(Stars);
+  var layer3 = new PLayer(Stars);// draws the twinkling stars
   layer3.mode(RING);
   layer3.set_boundary(-400,1000);
 
-  var layer4 = new PLayer(Asteroids);
+  var layer4 = new PLayer(Asteroids);//draws the falling asteroids
   layer4.mode(SWIRL(2));
   layer4.set_boundary(350,1000)
   
@@ -43,19 +43,19 @@ function setup_layers(pScope) {
 function Moon(animation, pScope) {
  
   noFill()
-  stroke(255)
+  stroke(255)//white
   strokeWeight(0.05)
   circle (0,0,1200)
 
   strokeWeight(64)
-  stroke('#909090')
-  let x = r * cos(angle);
-  let y = r * sin(angle);
+  stroke('#909090')//grey 
+  let x = r * cos(angle);//calculation to get the moon to orbit 
+  let y = r * sin(angle);//same as above
   point(x, y);
-  angle += 0.25; //0.5 is optimal speed 
-  fill('#303B45')
+  angle += 0.25; //0.25 is optimal speed, 
+  fill('#303B45')//darker grey 
   noStroke()
-  ellipse(x - 10, y, 20, 20)
+  ellipse(x - 10, y, 20, 20)//ellipse's are for moon craters 
   ellipse(x + 15, y + 10, 15, 15)
   ellipse(x + 10, y - 15, 12.5, 12.5)
  
@@ -70,14 +70,15 @@ function Planet(x, y, animation, pScope) {
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
-  var start = color('#4287f5')
-  var end =  color('#000000')
-  let interA = lerpColor(start,end,0.3)
+  var start = color('#4287f5')//light blue 
+  var end =  color('#000000')// black
+  let interA = lerpColor(start,end,0.3)//lerpcolours to blend black and blue for different areas of atmosphere
   let interB = lerpColor(start,end,0.6)
   let interC = lerpColor(start,end,0.9)
 
+  //section for black holes 
   fill('#ff4e01') //orange
-  ellipse(0, -325 - animation.wave() * 10, 70, 35)
+  ellipse(0, -325 - animation.wave() * 10, 70, 35)//ecllipses are black holes and the wave is to wiggle them
   fill('#FFFF00') //yellow 
   ellipse(0, -325 - animation.wave() * 10, 60, 25)
   fill(0) // black
@@ -127,11 +128,11 @@ function Planet(x, y, animation, pScope) {
 function Stars (x,y, animation, pScope){
 //offset with animation.wave or different frequency 
   
-
+  //sectipn for stars 
   strokeWeight(15 - animation.wave()* 15) //star twinkle 
   stroke(255)//white
-  scale (1 + animation.wave() * 0.01)
-  point((StarLocationX[2]),(StarLocationY[3]))
+  scale (1 + animation.wave() * 0.01)//star pulsation 
+  point((StarLocationX[2]),(StarLocationY[3]))//this line and below for stars 
   point((StarLocationX[1]),(StarLocationY[2]))
   point((StarLocationX[0]),(StarLocationY[4]))
   point((StarLocationX[3]),(StarLocationY[1]))
@@ -139,13 +140,15 @@ function Stars (x,y, animation, pScope){
 }
 
 function Asteroids (x,y, animation, pScope){
-  var outer = color('#5f4e43')
-  var inner = color('#FC8A17')
-  let asteroidColour = lerpColor(inner,outer,animation.frame * 10)
- scale(1 + animation.frame * 0.01)
- fill('#12c5e0')
+
+  //section for asteroids
+  var outer = color('#5f4e43')//dark grey 
+  var inner = color('#FC8A17')//orange
+  let asteroidColour = lerpColor(inner,outer,animation.frame * 10)//swap change colour from grey to orange near center
+ scale(1 + animation.frame * 0.01)//to keep the size constant througout the gif
+ fill('#12c5e0')//blue trail 
  ellipse(x,y-10,45,65)
- fill(asteroidColour)
- ellipse(x,y,40,40)
+ fill(asteroidColour)//the lerp colour changer 
+ ellipse(x,y,40,40)//asteroid 
  
 }
